0617f0492405eacfdbe04435873d56f36989ea10
dimarub2000 <dimarub2000@gmail.com>
2021-06-07 21:14:24 +0300

    add src and images

---
+++ src/len_hist.R
@@ -0,0 +1,28 @@
+
+source('lib.R')
+
+###
+
+NAME <- 'H3K4me3_MEL.ENCFF825RVT.mm10'
+#NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19'
+#NAME <- 'H3K4me3_A549.ENCFF573MUH.hg38'
+#NAME <- 'H3K4me3_A549.ENCFF832EOL.hg19'
+#NAME <- 'H3K4me3_A549.ENCFF832EOL.hg38'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+
+print(OUT_DIR)
+print(paste0('len_hist.', NAME, '.pdf'))
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
+
---
+++ src/lib.R
@@ -0,0 +1,17 @@
+# install.packages("ggplot2")
+# install.packages("dplyr")
+# install.packages("tidyr")
+# install.packages("tibble")
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+
+###
+
+# setwd("/home/dimarub2000/hse21_H3K4me3_ZDNA_mouse/src")
+
+DATA_DIR <- '/home/dimarub2000/hse21_H3K4me3_ZDNA_mouse/data/'
+OUT_DIR <- '/home/dimarub2000/hse21_H3K4me3_ZDNA_mouse/images/'
+
