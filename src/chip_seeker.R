c35efd22f67c69a6e8ba28d27d8948e00572dd34
dimarub2000 <dimarub2000@gmail.com>
2021-06-08 22:23:01 +0300

    fix bug in script

--- src/chip_seeker.R
+++ src/chip_seeker.R
@@ -12,7 +12,7 @@ NAME <- 'mouseZ-DNA1'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
 txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
-peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Mm.eg.db")
 
 pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
 
